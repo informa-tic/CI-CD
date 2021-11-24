8b00a2b41745c04d210290ae694b045690a9ca00
Laura Tortosa <laura.tortosa@sansarushop.com>
2021-11-24 16:05:20 +0100

    test10

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  // ...
+  testEnvironment: "node",
   moduleFileExtensions: ["ts", "js"],
-  // ...
-};
+  testResultsProcessor: "jest-sonar-reporter",
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  modulePathIgnorePatterns: ["mocks"]
+}
