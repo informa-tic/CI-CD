16aad547be11edc0696ebf41152d2ca5f6dd7621
Laura Tortosa <laura.tortosa@sansarushop.com>
2021-11-24 10:49:37 +0100

    added mult function

--- duplicateCode.ts
+++
@@ -1,7 +0,0 @@
-function sum(num1, num2): number {
-    return num1 + num2;
-}
-
-function mult(num1, num2): number {
-    return num1 * num2;
-}
--- src/duplicateCode.ts
+++ src/duplicateCode.ts
@@ -1,3 +1,6 @@
 export function sum(var1, var2): number {
     return var1 + var2;
 }
+export function mult(var1, var2): number {
+    return var1 * var2;
+}
