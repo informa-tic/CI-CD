80daee5d383a6ae0db751926295ff0f1ac2b8360
Laura Tortosa <laura.tortosa@sansarushop.com>
2021-11-24 12:07:40 +0100

    test coverage

--- __tests__/duplicateCode.test.ts
+++ __tests__/duplicateCode.test.ts
@@ -1,5 +1,12 @@
-import {sum} from '../src/duplicateCode'
+import {sum, mult, rest} from '../src/duplicateCode'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+  test('adds 1 * 2 to equal 3', () => {
+    expect(mult(1, 2)).toBe(2);
+  });
+
+  test('adds 1 -. 2 to equal 3', () => {
+    expect(rest(1, 2)).toBe(-1);
   });
