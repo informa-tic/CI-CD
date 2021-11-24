0df7d556d6008cdb36cadef76e5fbc1bcacd8c6a
Laura Tortosa <laura.tortosa@sansarushop.com>
2021-11-24 11:28:08 +0100

    added rest

--- src/duplicateCode.ts
+++ src/duplicateCode.ts
@@ -4,3 +4,6 @@ export function sum(var1, var2): number {
 export function mul(var1, var2): number {
     return var1 * var2;
 }
+export function rest(var1, var2): number {
+    return var1 - var2;
+}
